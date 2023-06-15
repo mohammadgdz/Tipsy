be6299709b8c55546f34be8e09d80409af1c8e06
mohammadgdz <mhd.gdz@gmail.com>
2023-06-15 20:44:27 +0330

    stepper update the splitNumberLabel update numberOfPeople

--- Tipsy/Controller/CalculatorViewController.swift
+++ Tipsy/Controller/CalculatorViewController.swift
@@ -12,35 +12,47 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
     @IBOutlet weak var twentyPctButton: UIButton!
-    @IBOutlet weak var splitNumberLabel: UIView!
     
     var tip = 0.0
+    var numberOfPeople = 2
+    var bilTotal = 0.0
+    var finalResult = "0.0"
 
     @IBAction func tipChanged(_ sender: UIButton) {
+        
         //Deselect all button that triggered
         zeroPctButton.isSelected = false
         tenPctButton.isSelected = false
         twentyPctButton.isSelected = false
+        
         //Select pressed button
         sender.isSelected = true
+        
         //get current title of pressed button
         let buttonTitle = sender.currentTitle!
+        
         //remove last string character (%) from current title of pressed button
         let buttonTitleMinusPercnetSign = String(buttonTitle.dropLast())
+        
         //turn string into a double
         let buttonTitleAsNumber = Double(buttonTitleMinusPercnetSign)!
+        
         //divide the percent
         tip = buttonTitleAsNumber / 100
 
     }
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
-        
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+        numberOfPeople = Int(sender.value)
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
         
         print(tip)
+        
+        print(numberOfPeople)
     }
     
 
